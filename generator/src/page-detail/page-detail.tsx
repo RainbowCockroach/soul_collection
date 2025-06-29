cc2ad741ee20bc0e7c9abe6418f4b37cca41378f
Lê Hồng Vân <voidrainbowcockroach@gmail.com>
2025-06-30 04:54:39 +0700

    placeholder page detail

---
+++ generator/src/page-detail/page-detail.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageDetail: React.FC = () => {
+  return (
+    <div>
+      <h1>Character Detail</h1>
+      <p>This is a placeholder for the character detail page.</p>
+    </div>
+  );
+};
+
+export default PageDetail;
