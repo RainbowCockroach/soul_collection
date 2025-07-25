dac9476e14ea006d9533467e44ca1c4e13507f03
VanLH23 <vanlh23@fpt.com>
2025-07-25 23:34:24 +0700

    Update Sam update script

--- SamClickHereToUpdate.bat
+++ SamClickHereToUpdate.bat
@@ -5,7 +5,10 @@ for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "d
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 set "datestamp=%YYYY%-%MM%-%DD% %HH%:%Min%:%Sec%"
+git pull origin sam
 git commit -m "sam update content %datestamp%"
 git push origin sam
+echo Creating pull request from sam to main...
+gh pr create --base main --head sam --title "Sam content update %datestamp%" --body "Automated content update from Sam on %datestamp%"
 echo *************DONE*************
 pause
