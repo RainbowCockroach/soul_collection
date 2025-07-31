bcdaba9be9739a5cd2f01cdfb1bdc49a735a403c
Lê Hồng Vân <voidrainbowcockroach@gmail.com>
2025-07-31 21:49:23 +0700

    straight push to main

--- SamClickHereToUpdate.bat
+++ SamClickHereToUpdate.bat
@@ -8,5 +8,15 @@ set "datestamp=%YYYY%-%MM%-%DD% %HH%:%Min%:%Sec%"
 git pull origin sam
 git commit -m "sam update content %datestamp%"
 git push origin sam
+if %errorlevel% neq 0 (
+    echo Push to sam branch failed. Stopping here.
+    pause
+    exit /b 1
+)
+git checkout main
+git merge sam
+git push origin main
+git checkout sam
+
 echo *************DONE*************
 pause
