087b0c322cab7ed46ee8ef87b5c534234025a0b7
Lê Hồng Vân <voidrainbowcockroach@gmail.com>
2025-07-31 22:23:28 +0700

    Fix script

--- SamClickHereToUpdate.bat
+++ SamClickHereToUpdate.bat
@@ -1,12 +1,8 @@
 @echo off
 cd /d "%~dp0"
 git add src/data/*
-for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
-set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
-set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
-set "datestamp=%YYYY%-%MM%-%DD% %HH%:%Min%:%Sec%"
 git pull origin sam
-git commit -m "sam update content %datestamp%"
+git commit -m "sam update content"
 git push origin sam
 git stash
 git checkout main
