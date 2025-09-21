bf65c17a00b44edfcc22b3c394d277d56a197f57
Lê Hồng Vân <voidrainbowcockroach@gmail.com>
2025-09-21 13:53:47 +0700

    Add effect to button

---
+++ src/common-components/ButtonWrapper.tsx
@@ -0,0 +1,59 @@
+import React, { ReactNode, useState } from "react";
+import useSound from "use-sound";
+import buttonSound from "/public/sound-effect/button.mp3";
+import "./ButtonWrapper.css";
+
+interface ButtonWrapperProps {
+  children: ReactNode;
+  onClick: () => void;
+  disabled?: boolean;
+  className?: string;
+}
+
+const ButtonWrapper: React.FC<ButtonWrapperProps> = ({
+  children,
+  onClick,
+  disabled = false,
+  className = "",
+}) => {
+  const [isPressed, setIsPressed] = useState(false);
+  const [playSound] = useSound(buttonSound, { volume: 0.5 });
+
+  const handleMouseDown = () => {
+    if (!disabled) {
+      setIsPressed(true);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsPressed(false);
+  };
+
+  const handleMouseLeave = () => {
+    setIsPressed(false);
+  };
+
+  const handleClick = () => {
+    if (!disabled) {
+      playSound();
+      onClick();
+    }
+  };
+
+  return (
+    <button
+      className={`button-wrapper ${className} ${isPressed ? "pressed" : ""} ${
+        disabled ? "disabled" : ""
+      }`}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ButtonWrapper;
