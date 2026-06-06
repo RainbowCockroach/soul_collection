9df00c0c996663bbca4669a74d5a333b1f9765a5
VanLH23 <vanlh23@fpt.com>
2026-06-07 01:00:05 +0700

    Footer social

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,27 +1,42 @@
-import React, { useState } from "react";
-import ButtonWrapper from "../common-components/ButtonWrapper";
-import BugReportDialog from "../bug-report/BugReportDialog";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import "./Footer.css";
 
+interface SocialLink {
+  name: string;
+  url: string;
+  icon: typeof faInstagram;
+}
+
+// Add more socials here as needed.
+const socialLinks: SocialLink[] = [
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/_its_sa.m_/",
+    icon: faInstagram,
+  },
+];
+
 const Footer: React.FC = () => {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-
   return (
-    <>
-      <footer className="site-footer">
-        <ButtonWrapper
-          onClick={() => setIsDialogOpen(true)}
-          className="bug-report-button"
-        >
-          Report a Bug
-        </ButtonWrapper>
-      </footer>
-
-      <BugReportDialog
-        isOpen={isDialogOpen}
-        onClose={() => setIsDialogOpen(false)}
-      />
-    </>
+    <footer className="site-footer">
+      <nav className="social-links" aria-label="Social media">
+        {socialLinks.map((social) => (
+          <a
+            key={social.name}
+            href={social.url}
+            className="social-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={social.name}
+            title={social.name}
+          >
+            <FontAwesomeIcon icon={social.icon} />
+          </a>
+        ))}
+      </nav>
+    </footer>
   );
 };
 
